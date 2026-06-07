bca984a6e7b7bbedff96fe70238dfbaac58981f8
hauan <hauankawai@gmail.com>
2026-06-07 14:51:00 -0300

    create main loop connecting class menu and game classes

--- Code/menu.py
+++ Code/menu.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from idlelib import window
+
 
 class Menu:
-    def __init__(self):
-        self.window = None
+    def __init__(self, window):
+        self.window = window
 
     def run(self, ):
         pass
